4fb820b9b585e9180322d25caeae321938021b5a
RaymondHe <2224928976@qq.com>
2023-08-09 21:29:43 +0800

    Init.

---
+++ Assets/CUBE/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float JumpForce;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, JumpForce, 0));
+        }
+    }
+}
